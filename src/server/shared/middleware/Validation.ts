215525a6b5ff13893b2c350483ac7f23a3926d98
Bronze <joaobronze@sisconsultoria.com.br>
2023-12-30 19:49:48 -0300

    node 16

--- src/server/shared/middleware/Validation.ts
+++ src/server/shared/middleware/Validation.ts
@@ -1,13 +1,13 @@
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { SchemaOf, ValidationError } from 'yup';
+import { Schema, ValidationError } from 'yup';
 
 
 type TProperty = 'body' | 'header' | 'params' | 'query';
 
-type TGetSchema = <T>(schema: SchemaOf<T>) => SchemaOf<T>
+type TGetSchema = <T>(schema: Schema<T>) => Schema<T>
 
-type TAllSchemas = Record<TProperty, SchemaOf<any>>;
+type TAllSchemas = Record<TProperty, Schema<any>>;
 
 type TGetAllSchemas = (getSchema: TGetSchema) => Partial<TAllSchemas>;
 
