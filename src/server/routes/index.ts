755299f8eacc0dec1c9b025b34463ccb47eb4d16
Bronze <joaobronze@sisconsultoria.com.br>
2024-01-01 16:47:08 -0300

    feature/controller-cidade

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -12,8 +12,12 @@ router.get('/', (_, res) => {
   return res.send('Olá, DEV!');
 });
 
-router.post('/cidades', CidadesController.createValidation, CidadesController.create);
 router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
+router.post('/cidades', CidadesController.createValidation, CidadesController.create);
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
+
 
 
 export { router };
